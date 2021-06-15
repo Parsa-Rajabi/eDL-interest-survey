f3b4cd8ce7f2a4b42b11589753e86da52218f808
Parsa <ParsaRajabiPR@gmail.com>
2021-06-15 12:07:13 -0700

    Removed React Icon

--- interest-survey/src/App.js
+++ interest-survey/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import SurveyComponent from "./SurveyComponent";
 
 function App() {
     return (
         <div className="App">
-            <img src={logo} className="App-logo" alt="logo"/>
             <SurveyComponent/>
         </div>
     );
