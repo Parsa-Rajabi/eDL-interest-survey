e1e2ce705c64a267551142dbd4edcebd05c45295
Parsa <ParsaRajabiPR@gmail.com>
2021-06-28 15:50:44 -0700

    SurveyText: Added eDL logo

--- interest-survey/src/App.js
+++ interest-survey/src/App.js
@@ -4,6 +4,11 @@ import SurveyComponent from "./SurveyComponent";
 function App() {
     return (
         <div className="App">
+            <img src={"https://edynamiclearning.com/wp-content/uploads/2020/09/Logo_Vert_Graduate_Web.png"}
+                 width={100}
+                 alt={"eDynamic Learning Logo"}
+                 class={"image"}
+            />
             <h1>Interest Survey</h1>
             <SurveyComponent/>
         </div>
