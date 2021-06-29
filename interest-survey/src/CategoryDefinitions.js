5d0a9de1007723967ca06ed16d248f3dddebe4ba
Parsa <ParsaRajabiPR@gmail.com>
2021-06-29 00:05:29 -0700

    SurveyText: Added CategoryDefinitions for result page

---
+++ interest-survey/src/CategoryDefinitions.js
@@ -0,0 +1,28 @@
+/*
+*
+* @Author: Parsa Rajabi, ParsaRajabiPR [at] gmail.com
+* @Date: June 2021
+* @Description: Constants for Survey
+*
+* */
+
+
+export const CATEGORY_DEF = {
+    REALISTIC: "People with realistic interests like work that includes practical, hands-on activities.",
+    INVESTIGATIVE: "People with investigative interests like work that has to do with ideas and thinking.",
+    ARTISTIC: "People with artistic interests like work that harnesses creativity, such as acting, music, art and design, writing.",
+    SOCIAL: "People with social interests like working with others to help them learn and grow.",
+    ENTERPRISING: "People with enterprising interests like work that has to do with starting up and taking action to carry out business projects.",
+    CONVENTIONAL: "People with conventional interest like work with established routines and procedures. Conventional does not mean dull!",
+}
+
+export const CATEGORY_EXAMPLES = {
+    REALISTIC: ["Working with plants and animals", "Materials like wood, tools, machinery", "Working outdoors"],
+    INVESTIGATIVE: ["Searching for facts", "Figuring out problems"],
+    ARTISTIC: ["Work that stokes your creative fire", "Combining your artistic side with business roles"],
+    SOCIAL: ["Teaching", "Providing advice", "Helping others"],
+    ENTERPRISING: ["Persuading people", "Leading", "Making decisions", "Taking risks for profits"],
+    CONVENTIONAL: ["Working with clear rules", "Clarity about expectations"]
+}
+
+export const EXAMPLE_HEADER = "You might enjoy: "
